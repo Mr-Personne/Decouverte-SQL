080eab25fa1d9564914690fcaaed32ba3ca7a567
Mr-Personne <h.sacha07@gmail.com>
2019-12-12 14:49:15 +0000

    ajout requete sql

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php 
+
+try {
+    //dbname (database name)
+    $bdd = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8', 'root', '1234');
+
+    $reponse = $bdd->query('SELECT * FROM contact');
+    //ira chercher la premiere ligne de toute la table 'contact'
+    $donnee = $reponse->fetch();
+    // print_r($donnee);
+    for($i = 0; $i < 10; $i++){
+        echo '<p>Votre Prénom est : <strong>'.$donnee['first_name'].'</strong> et votre Nom : <strong>'.$donnee['last_name'].
+        '</strong>; Votre email est : <strong>'.$donnee['email'].'</strong> et votre Country Code est : <strong>'.$donnee['country_code'].'</strong></p>';
+        // echo '<p>Votre email est : '.$donnee['email'].' et votre Country Code est : '.$donnee['country_code'].'</p>';
+        // echo '<br>';
+        //a chaque fetch il va a la prochaine ligne de la table
+        $donnee = $reponse->fetch();
+    }
+
+    //termine la requete
+    $reponse->closeCursor();
+
+} catch (Exception $e) {
+    //verif si erreur et met un message contenu dans $e qui dévoilera moins que le message erreur de base
+    die('Erreur : ' . $e->getMessage());
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
