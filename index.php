968fa8023ca9ba8060819aaa7822a5151aee1a99
Mr-Personne <h.sacha07@gmail.com>
2019-12-13 09:23:48 +0000

    finished question 9 and changed php to be in accordance with 10

--- index.php
+++ index.php
@@ -2,19 +2,22 @@
 
 try {
     //dbname (database name)
-    $bdd = new PDO('mysql:host=localhost;dbname=contacts;charset=utf8', 'root', '1234');
+    $bdd = new PDO('mysql:host=localhost;dbname=acs;charset=utf8', 'root', '1234');
 
-    $reponse = $bdd->query('SELECT * FROM contact');
+    $reponse = $bdd->query('SELECT membres_promo.first_name, departements.depName FROM `membres_promo` INNER JOIN departements ON departements.depCode = membres_promo.zip_code ');
     //ira chercher la premiere ligne de toute la table 'contact'
     $donnee = $reponse->fetch();
     // print_r($donnee);
-    for($i = 0; $i < 10; $i++){
-        echo '<p>Votre Prénom est : <strong>'.$donnee['first_name'].'</strong> et votre Nom : <strong>'.$donnee['last_name'].
-        '</strong>; Votre email est : <strong>'.$donnee['email'].'</strong> et votre Country Code est : <strong>'.$donnee['country_code'].'</strong></p>';
-        // echo '<p>Votre email est : '.$donnee['email'].' et votre Country Code est : '.$donnee['country_code'].'</p>';
-        // echo '<br>';
-        //a chaque fetch il va a la prochaine ligne de la table
-        $donnee = $reponse->fetch();
+    // for($i = 0; $i < 10; $i++){
+    //     echo '<p>Votre Prénom est : <strong>'.$donnee['first_name'].'</strong> et votre Nom : <strong>'.$donnee['last_name'].
+    //     '</strong>; Votre email est : <strong>'.$donnee['email'].'</strong> et votre Country Code est : <strong>'.$donnee['country_code'].'</strong></p>';
+    //     // echo '<p>Votre email est : '.$donnee['email'].' et votre Country Code est : '.$donnee['country_code'].'</p>';
+    //     // echo '<br>';
+    //     //a chaque fetch il va a la prochaine ligne de la table
+    //     $donnee = $reponse->fetch();
+    // }
+    while ($donnee = $reponse->fetch()){
+        echo '<p>'.$donnee['first_name'].' est de '.$donnee['depName'].'</p>';
     }
 
     //termine la requete
